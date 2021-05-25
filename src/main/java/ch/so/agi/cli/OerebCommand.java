1338aded6bbb66075809eda007594eba2342fa71
edigonzales <edi.gonzales@gmail.com>
2021-05-25 21:05:13 +0200

    start tests / start http request

--- src/main/java/ch/so/agi/cli/OerebCommand.java
+++ src/main/java/ch/so/agi/cli/OerebCommand.java
@@ -1,14 +1,59 @@
 package ch.so.agi.cli;
 
-import picocli.CommandLine.Command;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URLEncoder;
+import java.net.http.HttpClient;
+import java.net.http.HttpHeaders;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.Callable;
+import java.net.http.HttpClient.Redirect;
+import java.net.http.HttpClient.Version;
+import java.net.http.HttpRequest.Builder;
 
-@Command(name = "oereb", description = "Search questions matching criteria.", 
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Model.CommandSpec;
+import picocli.CommandLine.Spec;
+
+@Command(name = "oereb", description = "Search parcels by number and town and retrieve links to the plr extract.", 
     mixinStandardHelpOptions = true)
-public class OerebCommand implements Runnable {
+public class OerebCommand implements Callable<Integer> {
+    @Spec 
+    CommandSpec spec;
     
-    @Override
-    public void run() {
+    private final static String SEARCH_SERVICE_URL = "https://geo.so.ch/api/search/v2/";
+    private final static String FILTER = "ch.so.agi.av.grundstuecke.rechtskraeftig";
+    
+    public Integer call() {
         System.out.println("Run subcommand...");
-        System.out.println("bar");
+
+        HttpResponse<String> response = null;
+        try {
+
+            String searchText = "messen 168";
+            String encodedSearchText = URLEncoder.encode(searchText, StandardCharsets.UTF_8.toString());
+            String requestUrl = SEARCH_SERVICE_URL + "?searchtext="+encodedSearchText+"&filter="+FILTER+"&limit=5";
+            System.out.println(requestUrl);
+        
+            HttpClient httpClient = HttpClient.newBuilder().version(Version.HTTP_1_1).followRedirects(Redirect.ALWAYS)
+                    .build();
+            Builder requestBuilder = HttpRequest.newBuilder();
+            requestBuilder.GET().uri(URI.create(requestUrl));
+            
+            HttpRequest request = requestBuilder.build();
+            response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+            return 1;
+        }
+        System.out.println(response.body());
+        
+        System.out.println("Hallo ÖREB");
+        
+        spec.commandLine().getOut().println("Your output is abc...");
+        
+        return 0;
     }
 }
--- src/test/java/ch/so/agi/cli/AppTest.java
+++ src/test/java/ch/so/agi/cli/AppTest.java
@@ -1,12 +1,34 @@
 package ch.so.agi.cli;
 
 import org.junit.jupiter.api.Test;
+
+import picocli.CommandLine;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
 
 class AppTest {
     @Test 
     void appHasAGreeting() {
 //        App classUnderTest = new App();
 //        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+        
+        App app = new App();
+        CommandLine cmd = new CommandLine(app);
+
+//        String[] args = "my command args".split(" ");
+//        new CommandLine(new MyApp()).execute(args);
+//        assertEquals("MY COMMAND OUTPUT", out.toString
+        
+        StringWriter sw = new StringWriter();
+        cmd.setOut(new PrintWriter(sw));
+        
+        int exitCode = cmd.execute("oereb");
+        assertEquals(0, exitCode);
+        assertEquals("Your output is abc...", sw.toString());
+
+
     }
 }
